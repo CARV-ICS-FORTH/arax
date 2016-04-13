636bfe6836ce541feadd4ee55f1d8f31c363da79
dmcbeing <dmcbeing@hotmail.com>
2016-04-13 15:08:26 +0300

    Add names to accelerators
    Copy the accelerator name in vine_accel_init() and add relevant function
    vine_accel_get_name() to retrieve the name.

--- src/core/vine_accel.c
+++ src/core/vine_accel.c
@@ -8,9 +8,16 @@ vine_accel_s* vine_accel_init(void *mem, char *name, vine_accel_type_e type)
 	utils_list_node_init( &(obj->list) );
 	obj->owner = 0;
 	obj->type  = type;
+	memcpy(obj->name,name,strlen(name));
+	return obj;
 }
 
 size_t vine_accel_calc_size(char *name)
 {
 	return sizeof(vine_accel_s)+strlen(name);
+}
+
+const char * vine_accel_get_name(vine_accel_s* accel)
+{
+	return accel->name;
 }
