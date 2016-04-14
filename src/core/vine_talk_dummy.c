46b7c89741e70b1546d2312314a0e47dd159ab9f
alexis <akavroulakis@gmail.com>
2016-04-14 15:56:28 +0300

    changes inorder to fix compile errors

--- src/core/vine_talk_dummy.c
+++ src/core/vine_talk_dummy.c
@@ -38,40 +38,41 @@ vine_accel_loc_s vine_accel_location(vine_accel * accel)
 	vine_accel_loc_s return_val;
 	int task_duration=0;
 	log_vine_accel_location( accel, __FUNCTION__,return_val,task_duration);
+	return return_val;
 }
 
 vine_accel_type_e vine_accel_type(vine_accel * accel)
 {
 
 	int task_duration = 0;
-	vine_accel_type_e return_value;
+	vine_accel_type_e return_value =0;
 	log_vine_accel_type(accel,__FUNCTION__,return_value,&task_duration);
-
-
+	return return_value;
 }
 
 vine_accel_state_e vine_accel_stat(vine_accel * accel,vine_accel_stats_s * stat)
 {
 
 	int task_duration = 0;
-	vine_accel_state_e return_value;
+	vine_accel_state_e return_value = 0;
 	log_vine_accel_stat(accel,stat,__FUNCTION__,task_duration,&return_value);
+	return return_value;
 }
 
 int vine_accel_acquire(vine_accel * accel)
 {
 
-	vine_accel_loc_s return_value;
+	int return_value = 0;
 	int task_duration = 0;
 	log_vine_accel_acquire(accel,__FUNCTION__,return_value,task_duration);
+	return 0;
 }
 
 void vine_accel_release(vine_accel * accel)
 {
 
 	int task_duration = 0;
-	vine_accel_loc_s return_value;
-	log_vine_accel_release(accel,__FUNCTION__,return_value,task_duration);
+	log_vine_accel_release(accel,__FUNCTION__,task_duration);
 }
 
 vine_proc * vine_proc_register(vine_accel_type_e type,const char * func_name,const void * func_bytes,size_t func_bytes_size)
@@ -79,18 +80,19 @@ vine_proc * vine_proc_register(vine_accel_type_e type,const char * func_name,con
 
 
 	int task_duration = 0;
-	vine_proc* return_value;
+	vine_proc* return_value = NULL;
 
 	log_vine_proc_register(type,func_name,
 						func_bytes,func_bytes_size,__FUNCTION__,
 						task_duration,return_value);
 
+	return return_value;
 }
 
 vine_proc * vine_proc_get(vine_accel_type_e type,const char * proc_name)
 {
 
-	vine_proc* ret_proc;
+	vine_proc* ret_proc = NULL;
 	int task_duration = 0;
 	log_vine_proc_get(type,proc_name,__FUNCTION__,task_duration,ret_proc);
 	return ret_proc;
@@ -99,15 +101,16 @@ vine_proc * vine_proc_get(vine_accel_type_e type,const char * proc_name)
 
 int vine_proc_put(vine_proc * func)
 {
-	int return_value;
+	int return_value = 0;
 	int task_duration = 0;
 	log_vine_proc_put(func,__FUNCTION__, task_duration,return_value);
+	return return_value;
 }
 
 vine_data * vine_data_alloc(size_t size,vine_data_alloc_place_e place)
 {
 	int task_duration=0;
-	vine_data* return_val;
+	vine_data* return_val = NULL;
 	log_vine_data_alloc(size,place,task_duration,__FUNCTION__,return_val);
 
 	return (vine_data *)malloc(size);
@@ -123,13 +126,12 @@ size_t vine_data_size(vine_data * data)
 void * vine_data_deref(vine_data * data)
 {
 
-
-
 	int task_duration=0;
-	void* return_val;
+	void* return_val = NULL;
 
 	log_vine_data_deref(data,__FUNCTION__,task_duration,return_val);
 
+	return return_val;
 
 }
 
@@ -140,8 +142,6 @@ void vine_data_mark_ready(vine_data * data)
 
 void vine_data_free(vine_data * data)
 {
-
-
 	int task_duration=0;
 	log_vine_data_free( data,__FUNCTION__,task_duration);
 	free(data);
@@ -152,8 +152,8 @@ vine_task * vine_task_issue(vine_accel * accel,vine_proc * proc,vine_data *args,
 {
 
 	int task_duration=0;
-	vine_task* return_value;
-	log_vine_task_issue(accel, proc,args,input,output,__FUNCTION__,task_duration,return_value);
+	vine_task* return_value =NULL;
+	log_vine_task_issue(accel, proc,args,in_count,out_count,input,output,__FUNCTION__,task_duration,return_value);
 	return return_value;
 }
 
@@ -161,14 +161,16 @@ vine_task_state_e vine_task_stat(vine_task * task,vine_task_stats_s * stats)
 {
 
 	int task_duration=0;
-	vine_task_state_e  return_value;
+	vine_task_state_e  return_value =-1;
 
 	log_vine_task_stat(task,stats,__FUNCTION__,task_duration,return_value);
+	return return_value;
 }
 
 vine_task_state_e vine_task_wait(vine_task * task)
 {
 	int task_duration=0;
-	vine_task_state_e return_val;
+	vine_task_state_e return_val = 0;
 	log_vine_task_wait( task,__FUNCTION__,task_duration,return_val);
+	return return_val;
 }
