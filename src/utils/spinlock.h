5452e701277c16bb746032f345e941f9e5aba4bc
dmcbeing <dmcbeing@hotmail.com>
2016-04-19 15:00:08 +0300

    Fixes according to discussion

--- src/utils/spinlock.h
+++ src/utils/spinlock.h
@@ -1,5 +1,5 @@
-#ifndef VINE_SPIN_HEADER
-#define VINE_SPIN_HEADER
+#ifndef UTILS_SPINLOCK_HEADER
+#define UTILS_SPINLOCK_HEADER
 #include <assert.h>
 
 typedef volatile uint64_t utils_spinlock;
@@ -24,10 +24,13 @@ static inline void utils_spinlock_init(utils_spinlock * lock)
  */
 static inline void utils_spinlock_lock(utils_spinlock * lock)
 {
-	TRY_AGAIN:
-	while(*lock);			/* Maybe add relax()? */
-	if( !__sync_bool_compare_and_swap(lock,0,1) )
-		goto TRY_AGAIN;		/* Someone was faster */
+	do
+	{
+		while(*lock);			/* Maybe add relax()? */
+		if( __sync_bool_compare_and_swap(lock,0,1) )
+			break;				/* We got it */
+	}
+	while(1);					/* Try again */
 }
 
 /**
