b2852c39e679b9cd81789b8d44d669fd77462c9f
dmcbeing <dmcbeing@hotmail.com>
2016-04-25 20:08:51 +0300

    Added previously mentioned system.[ch] files.

---
+++ src/utils/system.c
@@ -0,0 +1,21 @@
+#include "system.h"
+#include <unistd.h>
+#include <pwd.h>
+
+size_t system_total_memory()
+{
+	size_t pages = sysconf(_SC_PHYS_PAGES);
+	size_t page_size = sysconf(_SC_PAGE_SIZE);
+	return pages * page_size;
+}
+
+char* system_home_path()
+{
+	uid_t         uid = getuid();
+	struct passwd *pw = getpwuid(uid);
+
+	if (!pw)
+		return 0;
+
+	return pw->pw_dir;
+}
