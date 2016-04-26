590622975d6ac9deac42fe75e7a67b33f2a8ccfc
dmcbeing <dmcbeing@hotmail.com>
2016-04-26 18:04:16 +0300

    Added extra checks in testing.h

--- tests/testing.h
+++ tests/testing.h
@@ -5,7 +5,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include "utils/system.h"
+
+static int __attribute__ ((unused)) test_file_exists(char * file)
+{
+	struct stat buf;
+	return !stat ( file, &buf );
+
+}
 
 /**
  * Backup current config ~/.vinetalk to ./vinetalk.bak.
@@ -13,8 +21,10 @@
 static void __attribute__ ((unused)) test_backup_config()
 {
 	char vtpath[1024];
+	printf("%s\n",__func__);
 	snprintf(vtpath,1024,"%s/.vinetalk",system_home_path());
-	ck_assert(!rename(vtpath,"vinetalk.bak")); /* Keep old file */
+	if(test_file_exists(vtpath))
+		ck_assert(!rename(vtpath,"vinetalk.bak")); /* Keep old file */
 }
 
 /**
@@ -23,9 +33,11 @@ static void __attribute__ ((unused)) test_backup_config()
 static void __attribute__ ((unused)) test_restore_config()
 {
 	char vtpath[1024];
+	printf("%s\n",__func__);
 	snprintf(vtpath,1024,"%s/.vinetalk",system_home_path());
 	ck_assert(!unlink(vtpath));					/* Remove test file*/
-	ck_assert(!rename("vinetalk.bak",vtpath));
+	if(test_file_exists("vinetalk.bak"))
+		ck_assert(!rename("vinetalk.bak",vtpath));
 }
 
 /**
@@ -37,6 +49,7 @@ static int __attribute__ ((unused)) test_open_config()
 {
 	int fd;
 	char vtpath[1024];
+	printf("%s\n",__func__);
 	snprintf(vtpath,1024,"%s/.vinetalk",system_home_path());
 	fd = open(vtpath,O_RDWR|O_CREAT,0666);
 	ck_assert_int_gt(fd,0);
