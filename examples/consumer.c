0b8fa1bfdb045db7bab6825bf0c97bcecbb92865
Stelios Mavridis <mavridis@ics.forth.gr>
2016-04-29 17:24:43 +0300

    Joined producer and ex3 files to one.

--- examples/consumer.c
+++ examples/consumer.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 void        *shm = 0;
 vine_pipe_s *vpipe;
@@ -34,18 +35,9 @@ int main(int argc, char *argv[])
 		printf("Got task (%p) %s(%s)!", msg, vine_accel_get_name(msg->accel),
 			   ((vine_proc_s*)msg->proc)->name);
 
-		int         start = msg->in_count;
-		int         end   = start + msg->out_count;
-		int         out;
-		vine_data_s *vdata;
-
-		for (out = start; out < end; out++) {
-			vdata =
-			        offset_to_pointer(vine_data_s*, vpipe,
-			                          msg->io[out]);
-			vine_data_mark_ready(vdata);
-		}
-
+		sprintf((char*)vine_data_deref(msg->io[2]),"%s",(char*)vine_data_deref(msg->io[1]));
+		strcat((char*)vine_data_deref(msg->io[2]),(char*)vine_data_deref(msg->io[0]));
+		vine_data_mark_ready(vine_data_deref(msg->io[2]));
 	}
 	return 0;
 }
