fae6902ce8719cdd67b0f8da7879fb7835c8945e
dmcbeing <dmcbeing@hotmail.com>
2016-05-04 15:06:18 +0300

    Added IO flags in vide data

--- src/core/vine_data.h
+++ src/core/vine_data.h
@@ -4,10 +4,16 @@
 typedef struct vine_data_s {
 	vine_data_alloc_place_e place;
 	size_t                  size;
+	size_t                  flags;
 	volatile size_t         ready;
 
 	/* Add status variables */
 } vine_data_s;
+
+typedef enum vine_data_io {
+	VINE_INPUT  = 1,
+	VINE_OUTPUT = 2
+}vine_data_io_e;
 
 vine_data_s* vine_data_init(void *mem, size_t size,
                             vine_data_alloc_place_e place);
