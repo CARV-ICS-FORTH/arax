85e86b5a9a7b580bde00f02a95e6bf5323ad3072
dmcbeing <dmcbeing@hotmail.com>
2016-05-11 15:05:27 +0300

    Changed list to double linked.

--- src/utils/list.c
+++ src/utils/list.c
@@ -5,47 +5,45 @@ utils_list_s* utils_list_init(void *mem)
 	utils_list_s *list = mem;
 
 	list->length = 0;
-	list->next   = 0;
-
+	utils_list_node_init(&(list->head));
 	return list;
+}
+
+void utils_list_node_add(utils_list_node_s *head, utils_list_node_s *node)
+{
+	head->next->prev = node;
+	node->next = head->next;
+	node->prev = head;
+	head->next = node;
 }
 
 void utils_list_add(utils_list_s *list, utils_list_node_s *node)
 {
-	node->next = list->next;
-	list->next = node;
+	utils_list_node_add(&(list->head),node);
 	list->length++;
 }
 
 utils_list_node_s* utils_list_del(utils_list_s *list, utils_list_node_s *node)
 {
-	utils_list_node_s *prev = (utils_list_node_s*)&(list->next);
-
-	if ( !(prev->next) )
-		return 0;
-	while (prev && prev->next != node) // Might want to change to d linked
-		                           // list
-		prev = prev->next;
-	if (!prev)
-		return 0;
-	prev->next = node->next; // Delete node
+	node->next->prev = node->prev;
+	node->prev->next = node->next;
 	list->length--;
 	return node;
 }
 
 int utils_list_to_array(utils_list_s *list, utils_list_node_s **array)
 {
-	utils_list_node_s *node = list->next;
+	utils_list_node_s *itr;
 
 	if (!array)
 		return list->length;
 
-	if (node) {
-		do {
-			array[0] = node;
+	if (list->length) {
+		utils_list_for_each(*list,itr)
+		{
+			*array = itr;
 			array++;
-			node = node->next;
-		} while (node);
+		}
 		return list->length;
 	}
 	array[0] = 0;
@@ -54,5 +52,6 @@ int utils_list_to_array(utils_list_s *list, utils_list_node_s **array)
 
 void utils_list_node_init(utils_list_node_s *node)
 {
-	node->next = 0;
+	node->next = node;
+	node->prev = node;
 }
