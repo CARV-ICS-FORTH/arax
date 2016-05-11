ed41e2bd623a615dc519c791ab94cb2d38d2e4b6
dmcbeing <dmcbeing@hotmail.com>
2016-05-11 15:25:21 +0300

    Introducing vine_objects.

---
+++ src/core/vine_object.c
@@ -0,0 +1,49 @@
+#include "vine_object.h"
+#include "utils/spinlock.h"
+#include <stdio.h>
+#include <string.h>
+static const char * type2str[VINE_TYPE_COUNT] =
+{
+"Physical Accelerators",
+"Virtual Accelerators",
+"Proceddures"
+};
+
+void vine_object_repo_init(vine_object_repo_s * repo)
+{
+	int r;
+	for(r = 0 ; r < VINE_TYPE_COUNT ; r++)
+	{
+		utils_list_init(&repo->repo[r].list);
+		utils_spinlock_init(&repo->repo[r].lock);
+	}
+}
+
+void vine_object_repo_exit(vine_object_repo_s * repo)
+{
+	int r;
+	int len;
+	for(r = 0 ; r < VINE_TYPE_COUNT ; r++)
+	{
+		len = repo->repo[r].list.length;
+		if(len)
+			fprintf(stderr,"%lu %*s still registered!\n",repo->repo[r].list.length,(int)(strlen(type2str[r])-(len==1)),type2str[r]);
+	}
+}
+
+void vine_object_register(vine_object_repo_s * repo,vine_object_s * obj,vine_object_type_e type,char * name)
+{
+	snprintf(obj->name,VINE_OBJECT_NAME_SIZE,"%s",name);
+	obj->type = type;
+	utils_spinlock_lock(&(repo->repo[type].lock));
+	utils_list_add(&(repo->repo[type].list),&(obj->list));
+	utils_spinlock_unlock(&(repo->repo[type].lock));
+}
+
+void vine_object_remove(vine_object_repo_s * repo,vine_object_s * obj)
+{
+	utils_spinlock_lock(&(repo->repo[obj->type].lock));
+	utils_list_del(&(repo->repo[obj->type].list),&(obj->list));
+	utils_spinlock_unlock(&(repo->repo[obj->type].lock));
+}
+
---
+++ src/core/vine_object.h
@@ -0,0 +1,35 @@
+#ifndef VINE_OBJECT_HEADER
+#define VINE_OBJECT_HEADER
+#include "utils/list.h"
+#include "utils/spinlock.h"
+
+#define VINE_OBJECT_NAME_SIZE 32
+
+typedef enum vine_object_type{
+	VINE_TYPE_PHYS_ACCEL,
+	VINE_TYPE_VIRT_ACCEL,
+	VINE_TYPE_PROC,
+	VINE_TYPE_COUNT
+}vine_object_type_e;
+
+typedef struct
+{
+	struct
+	{
+		utils_list_s list;
+		utils_spinlock lock;
+	}repo[VINE_TYPE_COUNT];
+}vine_object_repo_s;
+
+typedef struct {
+	vine_object_type_e type;
+	utils_list_node_s list;
+	char name[VINE_OBJECT_NAME_SIZE];
+}vine_object_s;
+
+void vine_object_repo_init(vine_object_repo_s * repo);
+void vine_object_repo_exit(vine_object_repo_s * repo);
+
+void vine_object_register(vine_object_repo_s * repo,vine_object_s * obj,vine_object_type_e type,char * name);
+void vine_object_remove(vine_object_repo_s * repo,vine_object_s * obj);
+#endif
