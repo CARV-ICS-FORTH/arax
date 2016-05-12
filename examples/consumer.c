4d3229b058c63ab45eec51c4123d6b3242e32562
dmcbeing <dmcbeing@hotmail.com>
2016-05-12 16:09:04 +0300

    Added singal handling in producer example.

--- examples/consumer.c
+++ examples/consumer.c
@@ -4,41 +4,65 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
-void        *shm = 0;
-vine_pipe_s *vpipe;
+
+
+volatile int run = 1;
+
+static void quit(int sig)
+{
+	run = 0;
+}
+
 
 int main(int argc, char *argv[])
 {
+	vine_pipe_s *vpipe;
 	vine_task_msg_s *msg;
+	vine_accel_s *acc;
 
 	vpipe = vine_pipe_get();
 
-	vine_accel_s *acc;
+	if(!vpipe)
+	{
+		printf("VineTalk initialization failed!\n");
+		return -1;
+	}
 
 	acc =
 	        arch_alloc_allocate( vpipe->allocator, vine_accel_calc_size(
 	                                     "FakeAccel1") );
 	acc = vine_accel_init(&(vpipe->objs), acc, "FakeAccel1", CPU);
 
-	while (1) {
+	if (signal(SIGINT, quit) == SIG_ERR) {
+		fputs("Could not set the signal handler.\n", stderr);
+		return -1;
+	}
+
+	printf("Consumer started.\n");
+	while (run) {
 		do {
 			printf( "Used:%d\n", utils_queue_used_slots(
 			                vpipe->queue) );
 			msg = (vine_task_msg_s*)utils_queue_pop(vpipe->queue);
 			sleep(1);
-		} while (!msg);
-		printf("Got task (%p) %s(%s)!", msg,
-		       vine_accel_get_name(
-		               msg->accel), ( (vine_proc_s*)msg->proc )->obj.name);
+		} while (!msg && run);
+		if(msg)
+		{
+			printf("Got task (%p) %s(%s)!", msg,
+				vine_accel_get_name(
+						msg->accel), ( (vine_proc_s*)msg->proc )->obj.name);
 
-		sprintf( (char*)vine_data_deref(
-		                 msg->io[2]), "%s",
-		         (char*)vine_data_deref(msg->io[1]) );
-		strcat( (char*)vine_data_deref(
-		                msg->io[2]),
-		        (char*)vine_data_deref(msg->io[0]) );
-		vine_data_mark_ready(msg->io[2]);
+			sprintf( (char*)vine_data_deref(
+							msg->io[2]), "%s",
+					(char*)vine_data_deref(msg->io[1]) );
+			strcat( (char*)vine_data_deref(
+							msg->io[2]),
+					(char*)vine_data_deref(msg->io[0]) );
+			vine_data_mark_ready(msg->io[2]);
+		}
 	}
+	printf("Consumer shutting down.\n");
 	return 0;
 }
