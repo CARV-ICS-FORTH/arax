2a40fa64dc8f1beddb4465bf2bd982365486c4b2
dmcbeing <dmcbeing@hotmail.com>
2016-05-13 18:54:09 +0300

    Added documentation.

--- src/core/vine_object.h
+++ src/core/vine_object.h
@@ -5,6 +5,9 @@
 
 #define VINE_OBJECT_NAME_SIZE 32
 
+/**
+ * Enumeration with available Vine Object Types.
+ */
 typedef enum vine_object_type{
 	VINE_TYPE_PHYS_ACCEL,
 	VINE_TYPE_VIRT_ACCEL,
@@ -13,6 +16,9 @@ typedef enum vine_object_type{
 	VINE_TYPE_COUNT
 }vine_object_type_e;
 
+/**
+ * Vine object repository struct, contains references to all Vineyard objects.
+ */
 typedef struct
 {
 	struct
@@ -22,18 +28,66 @@ typedef struct
 	}repo[VINE_TYPE_COUNT];
 }vine_object_repo_s;
 
+/**
+ * Vine Object super class, all Vine Objects have this as their first member.
+ */
 typedef struct {
 	utils_list_node_s list;
 	vine_object_type_e type;
 	char name[VINE_OBJECT_NAME_SIZE];
 }vine_object_s;
 
+/**
+ * Initialize an vine_object_repo_s instance on allocated pointer \c repo.
+ *
+ * \param repo An atleast sizeof(vine_object_repo_s) big buffer.
+ */
 void vine_object_repo_init(vine_object_repo_s * repo);
+
+/**
+ * Perform cleanup and exit time checks.
+ *
+ * Prints on stderr, the Objects still registered and thus considered as
+ * leaks.
+ *
+ * \param repo A valid vine_object_repo_s instance.
+ * \return Number of 'leaked' Vine Objects.
+ */
 int vine_object_repo_exit(vine_object_repo_s * repo);
 
+/**
+ * Vine Object 'Constructor'
+ *
+ * Register \c obj at \c repo.
+ *
+ * \param repo A valid vine_object_repo_s instance.
+ * \param obj The object to be registered.
+ * \param type Type of the new vine_object.
+ * \param name The name on the new vine_object.
+ */
 void vine_object_register(vine_object_repo_s * repo,vine_object_s * obj,vine_object_type_e type,const char * name);
+/**
+ * Remove \c obj from \c repo.
+ *
+ * \param repo A valid vine_object_repo_s instance.
+ * \param obj The object to be removed from the repo.
+ *
+ */
 void vine_object_remove(vine_object_repo_s * repo,vine_object_s * obj);
-
+/**
+ * Get a locked utils_list_s for traversing all objects of \c type.
+ *
+ * \param repo A valid vine_object_repo_s instance.
+ * \param type Type of objects contained in list.
+ */
 utils_list_s * vine_object_list_locked(vine_object_repo_s * repo,vine_object_type_e type);
+/**
+ * Unlock previously locked list.
+ * Parameters should be the same with the previous vine_object_list_locked()
+ * invocation.
+ *
+ * \param repo A valid vine_object_repo_s instance.
+ * \param type Type of objects contained in list.
+ */
 void vine_object_list_unlock(vine_object_repo_s * repo,vine_object_type_e type);
 #endif
