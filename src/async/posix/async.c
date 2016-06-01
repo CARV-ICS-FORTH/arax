3abd5d706f7d9dd2702dbf57969406243d5ab4c4
dmcbeing <dmcbeing@hotmail.com>
2016-06-01 16:04:46 +0300

    Fixed completion semantics.

--- src/async/posix/async.c
+++ src/async/posix/async.c
@@ -3,14 +3,16 @@
 void arch_async_completion_init(arch_async_completion_s * compl)
 {
 	pthread_mutex_init(&(compl->mutex),0);
+	pthread_mutex_lock(&(compl->mutex));
 }
 
 void arch_async_completion_complete(arch_async_completion_s * compl)
 {
-	pthread_mutex_lock(&(compl->mutex));
+	pthread_mutex_unlock(&(compl->mutex));
 }
 
 void arch_async_completion_wait(arch_async_completion_s * compl)
 {
-	pthread_mutex_unlock(&(compl->mutex));
+	pthread_mutex_lock(&(compl->mutex));
+
 }
