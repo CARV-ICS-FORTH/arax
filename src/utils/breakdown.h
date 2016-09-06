1a48283e625e0b61dc0f87e466da3c576cc30524
dmcbeing <dmcbeing@hotmail.com>
2016-09-06 12:51:25 +0300

    c++ fix

--- src/utils/breakdown.h
+++ src/utils/breakdown.h
@@ -22,6 +22,10 @@ typedef struct{
 
 #define UTILS_BREAKDOWN_INSTANCE(name) utils_breakdown_instance_s name
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* ifdef __cplusplus */
+
 void utils_breakdown_init_stats(utils_breakdown_stats_s * stats);
 
 void utils_breakdown_begin(utils_breakdown_instance_s * bdown,utils_breakdown_stats_s * stats);
@@ -31,6 +35,11 @@ void utils_breakdown_advance(utils_breakdown_instance_s * bdown);
 void utils_breakdown_end(utils_breakdown_instance_s * bdown);
 
 void utils_breakdown_write(const char *file,const char * description,utils_breakdown_stats_s * stats);
+
+#ifdef __cplusplus
+}
+#endif /* ifdef __cplusplus */
+
 #else
 
 #define UTILS_BREAKDOWN_STATS(name)
