d816b259aae274cdd5d3e8cc631989bcc1fc4fd9
dmcbeing <dmcbeing@hotmail.com>
2016-11-26 01:53:15 +0200

    debug

--- ci_scripts/prep.sh
+++ ci_scripts/prep.sh
@@ -1,5 +1,8 @@
 yes | pacman -Suy
 yes | pacman -S grep
+cat /etc/pacman.d/mirrorlist
 cat /etc/pacman.d/mirrorlist | grep Greece -A 1 > mirrorlist
+echo "AFTER"
+cat /etc/pacman.d/mirrorlist
 mv mirrorlist /etc/pacman.d/mirrorlist
 yes | pacman -S awk make cmake gcc htop tree check git
