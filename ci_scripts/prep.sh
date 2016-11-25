68e84fde70b3d61dbc960ae2e0710da556652834
dmcbeing <dmcbeing@hotmail.com>
2016-11-26 01:56:31 +0200

    mirors fix

--- ci_scripts/prep.sh
+++ ci_scripts/prep.sh
@@ -1,8 +1,7 @@
+# Greece Servers FTW
+echo 'Server = http://ftp.cc.uoc.gr/mirrors/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://ftp.ntua.gr/pub/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://foss.aueb.gr/mirrors/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://ftp.otenet.gr/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 yes | pacman -Suy
-yes | pacman -S grep
-cat /etc/pacman.d/mirrorlist
-cat /etc/pacman.d/mirrorlist | grep Greece -A 1 > mirrorlist
-echo "AFTER"
-cat /etc/pacman.d/mirrorlist
-mv mirrorlist /etc/pacman.d/mirrorlist
-yes | pacman -S awk make cmake gcc htop tree check git
+yes | pacman -S awk make cmake gcc htop tree check git grep
