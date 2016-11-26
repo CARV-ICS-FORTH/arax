afed980a441a13c52b2e1b42cc93cac6e5558de2
dmcbeing <dmcbeing@hotmail.com>
2016-11-26 03:16:01 +0200

    ?

--- ci_scripts/packages.sh
+++ ci_scripts/packages.sh
@@ -1,3 +1,6 @@
-ls /var/cache/pacman/pkg/
-tar  -C /var/cache/pacman/pkg/ xvf packages.tar 
-ls /var/cache/pacman/pkg/
+if [ $1 == 0 ]
+then
+	tar -cf packages.tar /var/cache/pacman/pkg/
+else
+	tar  -C /var/cache/pacman/pkg/ xf packages.tar 
+fi
