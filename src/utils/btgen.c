941becc55e86bd584abfcf2960547a7daae1fda8
dmcbeing <dmcbeing@hotmail.com>
2017-01-10 16:39:15 +0200

    Now capturing more signals, and properly rerouting to previous handler.

--- src/utils/btgen.c
+++ src/utils/btgen.c
@@ -10,8 +10,20 @@
 
 #include "conf.h"
 
-struct  sigaction prev_sig;
-struct  sigaction vine_sig;
+struct SigRemap
+{
+	struct  sigaction prev;
+	struct  sigaction vine;
+	int signum;
+};
+
+#define CAPRTURED_SIGNAL_COUNT 5
+
+int captured_signals[CAPRTURED_SIGNAL_COUNT] = 
+{SIGILL,SIGTRAP,SIGABRT,SIGBUS,SIGSEGV};
+
+struct SigRemap sig_remap[CAPRTURED_SIGNAL_COUNT];
+
 int bt_fd = -1;
 char bt_fname[64];
 
@@ -20,17 +32,19 @@ void bt_handler(int signum);
 void utils_bt_init()
 {
 	int cnt = 0;
-	vine_sig.sa_handler = bt_handler;
-	if(sigaction(SIGBUS,&vine_sig,&prev_sig) < 0)
+
+	for(cnt = 0 ; cnt < CAPRTURED_SIGNAL_COUNT ; cnt++)
 	{
-		fprintf(stderr,"Failed to register bt_handler for SIGBUS.");
-		return;
+		sig_remap[cnt].signum = captured_signals[cnt];
+		sig_remap[cnt].vine.sa_handler = bt_handler;
+
+		if(sigaction(SIGBUS,&(sig_remap[cnt].vine),&(sig_remap[cnt].prev)) < 0)
+		{
+			fprintf(stderr,"Failed to register bt_handler for SIGBUS.");
+			return;
+		}
 	}
-	if(sigaction(SIGSEGV,&vine_sig,&prev_sig) < 0)
-	{
-		fprintf(stderr,"Failed to register bt_handler for SIGSEGV.");
-		return;
-	}
+
 	while(bt_fd < 0)
 	{
 
@@ -48,7 +62,14 @@ void bt_handler(int signum)
 	int bt_size = 32;
 	bt_size = backtrace(bt, bt_size);
 	backtrace_symbols_fd(bt,bt_size,bt_fd);
-	exit(-1);
+	for(bt_size = 0 ; bt_size < CAPRTURED_SIGNAL_COUNT ; bt_size++)
+	{
+		if(sig_remap[bt_size].signum == signum)
+		{
+			sig_remap[bt_size].prev.sa_handler(signum);
+			break;
+		}
+	}
 }
 
 void utils_bt_exit()
