e2e71361f77deaf1ccf8826f2234b3c09da3710f
Stelios Mavridis <mavridis@ics.forth.gr>
2017-11-23 02:44:30 +0200

    Fixed cmake build.

--- vdf/Collector.h
+++ vdf/Collector.h
@@ -1,35 +1,47 @@
 #ifndef VDF_COLLECTOR_HEADER
 	#define VDF_COLLECTOR_HEADER
-	#include <Poco/Net/TCPServerConnection.h>
-	#include <Poco/Net/StreamSocket.h>
-	#include "Poco/Net/TCPServer.h"
-	#include <unordered_map>
-	#include <mutex>
-	#include <ostream>
-	#include "Sample.h"
-	#include "JobTrace.h"
+	#ifdef VINE_TELEMETRY
+		#include <Poco/Net/TCPServerConnection.h>
+		#include <Poco/Net/StreamSocket.h>
+		#include "Poco/Net/TCPServer.h"
+		#include <unordered_map>
+		#include <mutex>
+		#include <ostream>
+		#include "Sample.h"
+		#include "JobTrace.h"
 
-	class Collector : public Poco::Net::TCPServer, public Poco::Net::TCPServerConnectionFactory
-	{
-		public:
-			class CollectorConnection : public Poco::Net::TCPServerConnection
-			{
-				virtual void run();
-				public:
-					CollectorConnection(const Poco::Net::StreamSocket& s,Collector & collector);
-				private:
-					Collector & collector;
-			};
-		private:
-
-			uint64_t start_time;
-			std::mutex map_lock;
-			std::unordered_map<void*,JobTrace*> jobs;
-		public:
-			Collector(uint16_t port);
-			void rawDump(std::ostream & os);
-			static void generateTaskExecutionGraph(std::ostream & os,const std::vector<JobTrace*> & jobs);
-			void taskExecutionGraph(std::ostream & os);
-			virtual Poco::Net::TCPServerConnection* createConnection(const Poco::Net::StreamSocket& sock);
-	};
+		class Collector : public Poco::Net::TCPServer, public Poco::Net::TCPServerConnectionFactory
+		{
+			public:
+				class CollectorConnection : public Poco::Net::TCPServerConnection
+				{
+					virtual void run();
+					public:
+						CollectorConnection(const Poco::Net::StreamSocket& s,Collector & collector);
+					private:
+						Collector & collector;
+				};
+			private:
+	
+				uint64_t start_time;
+				std::mutex map_lock;
+				std::unordered_map<void*,JobTrace*> jobs;
+			public:
+				Collector(uint16_t port);
+				void rawDump(std::ostream & os);
+				static void generateTaskExecutionGraph(std::ostream & os,const std::vector<JobTrace*> & jobs);
+				void taskExecutionGraph(std::ostream & os);
+				virtual Poco::Net::TCPServerConnection* createConnection(const Poco::Net::StreamSocket& sock);
+		};
+	#else
+		#include <stdint.h>
+		class Collector
+		{
+			public:
+				Collector(uint16_t port){}
+				void rawDump(std::ostream & os){os << "Telemetry is disabled!";}
+				void start(){}
+				void stop(){}
+		};
+	#endif
 #endif
