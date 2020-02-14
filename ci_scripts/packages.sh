bb11595b6160e0dc83bdd008068e35d68090cf0a
dmcbeing <dmcbeing@hotmail.com>
2020-02-14 19:30:25 +0200

    Lucky???

--- ci_scripts/packages.sh
+++ ci_scripts/packages.sh
@@ -6,5 +6,14 @@ then
 else
 #	tar -xf packages.tar  -C /
 	pwd
-	bash prep.sh
+# Greece Servers FTW
+echo 'Server = http://ftp.cc.uoc.gr/mirrors/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://ftp.ntua.gr/pub/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://foss.aueb.gr/mirrors/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://ftp.otenet.gr/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+yes | pacman -Syy
+yes | pacman -S archlinux-keyring
+yes | pacman -Suy
+yes | pacman -S awk make cmake gcc htop tree check git grep wget
+
 fi
