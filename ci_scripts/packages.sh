e565140a6648d9f8608a5e27575446db71b4976e
dmcbeing <dmcbeing@hotmail.com>
2020-02-14 19:26:25 +0200

    Lucky?

--- ci_scripts/packages.sh
+++ ci_scripts/packages.sh
@@ -1,7 +1,9 @@
 if [ $1 == 0 ]
 then
-	df -h /var/cache/pacman/pkg/
-	tar -cf packages.tar /var/cache/pacman/pkg/
+#	du -h /var/cache/pacman/pkg/
+#	tar -cf packages.tar /var/cache/pacman/pkg/
 else
-	tar -xf packages.tar  -C /
+#	tar -xf packages.tar  -C /
+	pwd
+	bash prep.sh
 fi
