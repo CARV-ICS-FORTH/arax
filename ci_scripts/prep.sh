8a3545674f1df0ce37cb7e731a4cce96bacea195
dmcbeing <dmcbeing@hotmail.com>
2020-02-14 18:56:08 +0200

    Updated prep.sh

--- ci_scripts/prep.sh
+++ ci_scripts/prep.sh
@@ -3,6 +3,7 @@ echo 'Server = http://ftp.cc.uoc.gr/mirrors/linux/archlinux/$repo/os/$arch' > /e
 echo 'Server = http://ftp.ntua.gr/pub/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 echo 'Server = http://foss.aueb.gr/mirrors/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 echo 'Server = http://ftp.otenet.gr/linux/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+yes | pacman -Syy
 yes | pacman -S archlinux-keyring
 yes | pacman -Suy
 yes | pacman -S awk make cmake gcc htop tree check git grep wget
