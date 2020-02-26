31debfd6387076750885c355e68aa39131476907
dmcbeing <dmcbeing@hotmail.com>
2020-02-26 18:06:09 +0200

    Minor tweaks

--- noop/noop.cpp
+++ noop/noop.cpp
@@ -3,19 +3,27 @@
 #include "VineLibUtilsCPU.h"
 #include <cstring>
 
+#define MAGIC 1337
+
+void noop_op(char *in, char *out,int l)
+{
+	int c;
+	l-=2;
+	for(c = 0 ; l >= 0 ; l--,c++)
+		out[c] = in[l];
+	out[c] = 0;
+}
+
 #ifdef BUILD_SO
 vine_task_state_e noop(vine_task_msg_s * msg)
 {
 	int l = vine_data_size(msg->io[0]);
 	char * in = (char*)vine_data_deref(msg->io[0]);
 	char * out = (char*)vine_data_deref(msg->io[1]);
+	int magic = *(int*)vine_data_deref(msg->args);
+	printf("Magic:\"%d == %d\"\n",magic,MAGIC);
 	printf("Nooping input:\"%s\"\n",in);
-	char * data = (char*)vine_data_deref(msg->io[0]);
-	int c;
-	l-=2;
-	for(c = 0 ; l >= 0 ; l--,c++)
-		out[c] = in[l];
-	out[c] = 0;
+	noop_op(in,out,l);
 	printf("Nooping output:\"%s\"\n",out);
 	vine_data_modified(msg->io[1],SHM_SYNC);
 	vine_task_mark_done(msg,task_completed);
@@ -36,19 +44,21 @@ int main(int argc,char * argv[])
 	vine_proc * proc = vine_proc_get(CPU,"noop");
 	char noop[5] = "NOOP";
 	vine_task * task;
+	int magic = MAGIC;
 	vine_buffer_s io[1] = {VINE_BUFFER(&noop, strlen(noop)+1)};
 
 	vine_data_modified(io[0],USER_SYNC);
 
 	vine_data_sync_to_remote(accel,io[0],0);
 
-	task = vine_task_issue(accel, proc, 0, 0, 1, io, 1, io);
+	task = vine_task_issue(accel, proc, &magic, 4, 1, io, 1, io);
 
 	vine_task_wait(task);
 
 	vine_data_sync_from_remote(accel,io[0],1);
 	
 	fprintf(stderr,"Noop is \'%s\'\n",noop);
+	vine_data_free(io[0]);
 	vine_task_free(task);
 	vine_proc_put(proc);
 	vine_talk_exit();
