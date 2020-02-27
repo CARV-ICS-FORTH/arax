d486344d985e6a82030b034fefc898a6c02fcc27
Stelios Mavridis <mavridis@ics.forth.gr>
2020-02-27 02:36:42 +0200

    Updated noop to use different input,output buffers.

--- noop/noop.cpp
+++ noop/noop.cpp
@@ -39,25 +39,37 @@ VINE_PROC_LIST_END()
 #ifdef BUILD_MAIN
 int main(int argc,char * argv[])
 {
+	if(argc != 2)
+	{
+		fprintf(stderr,"Usage:\n\t%s <string>\n\n",argv[0]);
+		return 0;
+	}
 	vine_talk_init();
 	vine_accel * accel = vine_accel_acquire_type(CPU);
 	vine_proc * proc = vine_proc_get(CPU,"noop");
-	char noop[5] = "NOOP";
+	size_t size = strlen(argv[1])+1;
+	char * out = (char*)calloc(size,1);
+	char * temp = (char*)calloc(size,1);
 	vine_task * task;
 	int magic = MAGIC;
-	vine_buffer_s io[1] = {VINE_BUFFER(&noop, strlen(noop)+1)};
+	vine_buffer_s io[2] = {
+		VINE_BUFFER(argv[1], size),
+		VINE_BUFFER(out, size)
+	};
 
 	vine_data_modified(io[0],USER_SYNC);
 
 	vine_data_sync_to_remote(accel,io[0],0);
 
-	task = vine_task_issue(accel, proc, &magic, 4, 1, io, 1, io);
+	task = vine_task_issue(accel, proc, &magic, 4, 1, io, 1, io+1);
 
 	vine_task_wait(task);
 
-	vine_data_sync_from_remote(accel,io[0],1);
-	
-	fprintf(stderr,"Noop is \'%s\'\n",noop);
+	vine_data_sync_from_remote(accel,io[1],1);
+
+	fprintf(stderr,"Noop is   \'%s\'\n",out);
+	noop_op(argv[1],temp,size);
+	fprintf(stderr,"Should be \'%s\'\n",temp);
 	vine_data_free(io[0]);
 	vine_task_free(task);
 	vine_proc_put(proc);
