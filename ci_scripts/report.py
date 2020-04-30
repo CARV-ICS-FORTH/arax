0dc4609244eb62e7d4ff46192db854fda3ba6af4
mavridis <mavridis@ics.forth.gr>
2020-04-30 17:46:37 +0300

    Ci test

--- ci_scripts/report.py
+++ ci_scripts/report.py
@@ -1,3 +1,4 @@
+import datetime
 import gitlab
 import sys
 import os
@@ -9,12 +10,16 @@ branch = os.environ['CI_BUILD_REF_NAME']
 commit_sha = os.environ['CI_COMMIT_SHA']
 proj_url = os.environ['CI_PROJECT_URL']
 user_email = os.environ['GITLAB_USER_EMAIL']
+pipeline_id = os.environ['CI_PIPELINE_ID']
 
 def writeMessage(commit,msg):
 	commit.comments.create({'note': msg})
 
 def t(msg='-'*20):
 	return "| %-20s " % (msg,)
+
+def c():
+	return "| :%-18s: " % ('-'*18,)
 
 def StatusMark(status):
 	if status == 'success':
@@ -24,43 +29,78 @@ def StatusMark(status):
 	else:
 		return t(status)
 
-def calcDuration(status):
-	print(status)
-	return "---"
+def parseTime(t):
+	if type(t) == type(""):
+		return datetime.datetime.strptime(t[:21],'%Y-%m-%dT%H:%M:%S.%f')
+	else:
+		return t
+
+def statDuration(status):
+	return calcDuration(status.started_at,status.finished_at)
+
+def pipeDuration(pipe):
+	return calcDuration(pipe.created_at,pipe.updated_at)
+
+def calcDuration(start,end):
+	start = parseTime(start)
+	end = parseTime(end)
+
+	delta = end-start
+	ret = str(delta).rstrip('0')
+	prev = None
+	
+	while ret != prev:
+		prev = ret
+		ret = ret.lstrip('0:')
+	
+	return ret
 
 with gitlab.Gitlab(host, private_token=token) as gl:
 	vt = gl.projects.get(project)
 	commits = vt.commits.list(ref_name=branch)
 	tables = []
 	coverages = []
-	failed = False
+	fails = []
+
+	pipeline = vt.pipelines.get(pipeline_id)
+
+	print(pipeline)
+
 	for commit in commits[:2]:
+		failed = False
 		statuses = commit.statuses.list()
 		coverage = None
-		table = t("Stage")+t("Duration")+t("Status")+t("Link")+"|   \n"
-		table += t()+t()+t()+"|   \n"
+		table = t("Stage")+t("Status")+t("Duration")+t("Link")+"|   \n"
+		table += t()+t()+c()+t()+"|   \n"
+		last_job = parseTime(pipeline.created_at)
 		for status in statuses:
-			print(status,"\n")
 			if coverage is None:
 				coverage = status.coverage
-			if status.started_at != None and status.status != 'running':
-				table += t(status.name)+t(calcDuration(status))+StatusMark(status.status)+t("<a href='"+proj_url+"/-/jobs/"+str(status.id)+"'>View</a>")+"|   \n"
 			if status.status == 'failed':
 				failed = True
+			if status.started_at != None and status.status != 'running':
+				last_job = max(last_job,parseTime(status.finished_at))
+				table += t(status.name)+StatusMark(status.status)+t(statDuration(status))+t("<a href='"+proj_url+"/-/jobs/"+str(status.id)+"'>View</a>")+"|   \n"
+
+		table += t("Overall")+StatusMark("failed" if failed else "success")+t(calcDuration(pipeline.created_at,last_job))+t("<a href='"+proj_url+"/pipelines/"+str(pipeline.id)+"'>View</a>")+"|   \n"
 		tables.append(table)
 		if coverage == None:
 			coverage = 0
 		coverages.append(coverage)
+		fails.append(failed)
 		
 	msg = ""
 	
 	user = "@" + user_email.split('@')[0]
 	
-	if failed:
-		msg += "# %s your commit did not pass the tests!  \n" % (user,)
+	if fails[0] == True:
+		msg += "# Commit failed the tests!  \n"
 	else:
-		msg += "# %s your commit passed the tests!  \n" % (user,)
+		msg += "# Commit passed the tests!  \n"
 
+	msg += "#### Commit: %s  \n" % (commit_sha,)
+	msg += "#### User: %s  \n" % (user,)
+	msg += "#### Branch: %s  \n" % (branch,)
 	cov_delta = coverages[0]-coverages[1]
 	msg += "#### Coverage: %6.2f%% (%6.2f%%)  \n" % (coverages[0],cov_delta)
 	msg += tables[0]
--- ci_scripts/report.sh
+++
@@ -1,8 +0,0 @@
-import gitlab
-import sys
-
-gl = gitlab.Gitlab('https://carvgit.ics.forth.gr', job_token=os.environ['CI_JOB_TOKEN'])
-
-projects = gl.projects.list()
-for project in projects:
-    print(project)
