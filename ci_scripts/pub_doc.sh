0b5d58fcfd20287f484652b1a4fd9952b13abcdc
Stelios Mavridis <mavridis@ics.forth.gr>
2021-02-14 18:36:12 +0200

    docs: Fixed documentation issues
    - Added recursive flag in documentation zip generation
    - Cleanup old heroku documentation

--- ci_scripts/pub_doc.sh
+++ ci_scripts/pub_doc.sh
@@ -11,15 +11,20 @@ git config --global user.email ${GITLAB_USER_EMAIL}
 git config --global user.name ${GITLAB_USER_EMAIL}
 
 heroku git:clone -a vtdocs
-ls -la
 unzip build/docs/VinetalkDocs.zip &> /dev/null
 cd VinetalkDocs
 echo "<?php header('Location: index.html'); ?>" > index.php
 sed -i '/carvgit/d' index.html
 cd -
+cd vtdocs
+git rm -rq *
+cd -
 cp -r VinetalkDocs/* vtdocs/
 cd vtdocs
 echo '{}' > composer.json
-git add .
-git commit -m "Updated docs"
-git push heroku master
+git add --all .
+if [ "${CI_COMMIT_REF_NAME}" == "master" ]
+then
+	git commit -m "Updated docs"
+	git push heroku master
+fi
