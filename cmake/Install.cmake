fbeb0fddada985a4139f8717da54e67110f2d044
Stelios Mavridis <mavridis@ics.forth.gr>
2021-05-19 23:41:52 +0300

    refactor: CMake cleanup
    Did the following:
    
     - Added install commands to properly install vinetalk, when root permissions
       are available.
     - Replaced global include_directorys with individual target_include

---
+++ cmake/Install.cmake
@@ -0,0 +1,16 @@
+install(DIRECTORY ${CMAKE_BINARY_DIR}/include/  DESTINATION ${CMAKE_INSTALL_PREFIX}/include/vinetalk)
+
+install(TARGETS vine EXPORT vinetalk LIBRARY DESTINATION lib INCLUDES DESTINATION include)
+install(TARGETS vine_st EXPORT vinetalk LIBRARY DESTINATION lib INCLUDES DESTINATION include)
+
+if(TARGET vdf)
+install(TARGETS vdf DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
+endif()
+install(TARGETS vinegrind DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
+
+install(FILES ${CMAKE_SOURCE_DIR}/cmake/vinetalk-config.cmake DESTINATION ${CMAKE_INSTALL_PREFIX})
+install(EXPORT vinetalk DESTINATION ${CMAKE_INSTALL_PREFIX})
+
+export(EXPORT vinetalk
+    FILE "${CMAKE_CURRENT_BINARY_DIR}/vinetalk.cmake"
+)
