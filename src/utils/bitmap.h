1633c877c2c797402fbc9d91e6f8b85afe7fa55a
Stelios Mavridis <mavridis@ics.forth.gr>
2021-06-08 19:03:15 +0300

    docs: Updated bitmap documentation

--- src/utils/bitmap.h
+++ src/utils/bitmap.h
@@ -17,16 +17,58 @@ typedef struct utils_bitmap
 
 #define BITMAP_NOT_FOUND ((size_t) -1)
 
+/**
+ * Initialize bitmap starting in \c mem, holding \c size_bits bits.
+ *
+ * \note Ensure \c mem is greater than or equal to \c UTILS_BITMAP_CALC_BYTES()
+ *
+ * \param mem Pointer to memory of \c UTILS_BITMAP_CALC_BYTES(size_bits) or more
+ * \param size_bits Number of bits this bitmap will hold.
+ * \return Returns mem on success, null otherwise.
+ */
 utils_bitmap_s* utils_bitmap_init(void *mem, size_t size_bits);
 
+/**
+ * Returns number of bits \c bmp holds, same as the value given in \c utils_bitmap_init.
+ *
+ * \c bmp An initialized utils_bitmap_s instance.
+ * \return Bits contained in \c bmp
+ */
 size_t utils_bitmap_size(utils_bitmap_s *bmp);
 
+/**
+ * Returns number of bits currently used in \c bmp.
+ *
+ * \c bmp An initialized utils_bitmap_s instance.
+ * \return Number of allocated bits in \c bmp.
+ */
 size_t utils_bitmap_used(utils_bitmap_s *bmp);
 
+/**
+ * Returns number of unused bits in \c bmp.
+ *
+ * \note This is utils_bitmap_size() - utils_bitmap_used()
+ *
+ * \c bmp An initialized utils_bitmap_s instance.
+ * \return Number of free bits in \c bmp.
+ */
 size_t utils_bitmap_free(utils_bitmap_s *bmp);
 
+/**
+ * Allocate \c bits cotiguous bits from \c bmp and return index of first bit.
+ *
+ * \c bmp An initialized utils_bitmap_s instance.
+ * \return Bit index of allocation start.
+ */
 size_t utils_bitmap_alloc_bits(utils_bitmap_s *bmp, size_t bits);
 
+/**
+ * Free contiguous \c bits starting from \c start, from the \c bmp bitmap.
+ *
+ * \c bmp An initialized utils_bitmap_s instance.
+ * \c start First bit index to be freed.
+ * \c bits Number of bits to free.
+ */
 void utils_bitmap_free_bits(utils_bitmap_s *bmp, size_t start, size_t bits);
 
 /**
@@ -36,7 +78,7 @@ void utils_bitmap_free_bits(utils_bitmap_s *bmp, size_t start, size_t bits);
  *
  * \note This should only be used in tests, as it is very slow.
  *
- * \param bmp A valid utils_bitmap_s instance.
+ * \c bmp An initialized utils_bitmap_s instance.
  * \return Bits allocated(set) in \c bits array of \c bmp
  */
 size_t utils_bitmap_count_allocated(utils_bitmap_s *bmp);
@@ -45,6 +87,8 @@ size_t utils_bitmap_count_allocated(utils_bitmap_s *bmp);
  * Print the bitmap oc \c bmp, in stderr.
  *
  * \note Use this for debuging
+ *
+ * \c bmp An initialized utils_bitmap_s instance.
  */
 void utils_bitmap_print_bits(utils_bitmap_s *bmp);
 #endif // ifndef UTILS_BITMAP_HEADER
