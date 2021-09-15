a913691574e51be4e7d27cc6e755f0e9957d2b63
Stelios Mavridis <mavridis@ics.forth.gr>
2021-09-16 01:38:17 +0300

    test(core): Added additional size checks in struct_interop test

--- tests/struct_interop.h
+++ tests/struct_interop.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include "vine_pipe.h"
 #include "core/vine_data.h"
-#define STRUCT_INTEROP_SIZES 9
+#define STRUCT_INTEROP_SIZES 15
 
 /*
  * Both functions put the same sizeof(struct) results in \c sizes.
@@ -27,15 +27,21 @@ extern "C" void get_cpp_sizes(size_t *sizes)
 void get_c_sizes(size_t * sizes)
 #endif /* ifdef __cplusplus */
 {
-    sizes[0] = sizeof(vine_throttle_s);
-    sizes[1] = sizeof(vine_proc_s);
-    sizes[2] = sizeof(vine_accel_s);
-    sizes[3] = sizeof(vine_data_s);
-    sizes[4] = sizeof(vine_task_msg_s);
-    sizes[5] = sizeof(vine_pipe_s);
-    sizes[6] = sizeof(utils_queue_s);
-    sizes[7] = sizeof(vine_vaccel_s);
-    sizes[8] = sizeof(utils_list_s);
+    sizes[0]  = sizeof(async_semaphore_s);
+    sizes[1]  = sizeof(async_completion_s);
+    sizes[2]  = sizeof(async_condition_s);
+    sizes[3]  = sizeof(struct arch_alloc_s);
+    sizes[4]  = sizeof(utils_list_s);
+    sizes[5]  = sizeof(utils_queue_s);
+    sizes[6]  = sizeof(utils_kv_s);
+    sizes[7]  = sizeof(vine_throttle_s);
+    sizes[8]  = sizeof(vine_proc_s);
+    sizes[9]  = sizeof(vine_accel_s);
+    sizes[10] = sizeof(vine_data_s);
+    sizes[11] = sizeof(vine_task_stats_s);
+    sizes[12] = sizeof(vine_task_msg_s);
+    sizes[13] = sizeof(vine_pipe_s);
+    sizes[14] = sizeof(vine_vaccel_s);
 }
 
 #endif // ifndef TEST_STRUCT_INTEROP_HEADER
