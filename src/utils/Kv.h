7614f4ab411974cb24af1dffdc0f156e7ec53df2
dmcbeing <dmcbeing@hotmail.com>
2021-11-02 18:05:26 +0200

    fix(utils): Added extern C in kv.h

--- src/utils/Kv.h
+++ src/utils/Kv.h
@@ -9,31 +9,40 @@
  */
 typedef struct
 {
-	struct Pair
-	{
-		void * key;
-		void * value;
-	} kv[VINE_KV_CAP];
-	size_t pairs;
-	utils_spinlock lock;
+    struct Pair
+    {
+        void *key;
+        void *value;
+    }              kv[VINE_KV_CAP];
+    size_t         pairs;
+    utils_spinlock lock;
 } utils_kv_s;
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* ifdef __cplusplus */
 
 /**
  * Initialize \c kv.
  */
-void utils_kv_init(utils_kv_s * kv);
+void utils_kv_init(utils_kv_s *kv);
 
 /**
  * Set kv[key] = value.
  *
  * Overwrites existing value.
  */
-void utils_kv_set(utils_kv_s * kv,void * key,void * value);
+void utils_kv_set(utils_kv_s *kv, void *key, void *value);
 
 /**
  * Return &(kv[key]) if found.
  *
  * Returns NULL if \c key not found.
  */
-void ** utils_kv_get(utils_kv_s * kv,void * key);
-#endif
+void** utils_kv_get(utils_kv_s *kv, void *key);
+
+#ifdef __cplusplus
+}
+#endif /* ifdef __cplusplus */
+
+#endif // ifndef VINEYARD_KV_HEADER
