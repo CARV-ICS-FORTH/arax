73a23e9695486a323a66f7c1aeeb5703dc513cb8
Stelios Mavridis <mavridis@ics.forth.gr>
2022-02-23 19:03:17 +0200

    style: Removed cmake messages

--- cmake/Documentation.cmake
+++ cmake/Documentation.cmake
@@ -1,4 +1,4 @@
-find_package(Doxygen)
+find_package(Doxygen QUIET)
 
 if(DOXYGEN_FOUND)
   option(BUILD_DOCS "Build documentation" ON) # skip build_check
@@ -7,7 +7,6 @@ else()
 endif()
 
 if(BUILD_DOCS)
-  message(STATUS "Documentation generation enabled")
   if(DOXYGEN_DOT_FOUND)
     set(DOXYGEN_HAVE_DOT "YES")
   else()
@@ -21,6 +20,4 @@ if(BUILD_DOCS)
     DEPENDS docs_pre
     COMMENT "Doc files at: ${CMAKE_CURRENT_BINARY_DIR}/docs/html/index.html")
   add_custom_target(docs DEPENDS doc)
-else()
-  message(STATUS "Documentation generation disabled")
 endif()
