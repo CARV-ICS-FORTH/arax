20226dfa856fe9abeaf274e3de47cbd459684485
dmcbeing <dmcbeing@hotmail.com>
2023-04-06 12:18:00 +0300

    chore: Cleanup CMake
    - Hide advanced/seldomly used options
    - Remove redundant ARAX_CONTROLLER_PATH option

---
+++ cmake/hide_advanced_options.cmake
@@ -0,0 +1,8 @@
+mark_as_advanced(Poco_Foundation_INCLUDE_DIR)
+mark_as_advanced(Poco_Foundation_LIBRARY_DEBUG)
+mark_as_advanced(Poco_INCLUDE_DIRS)
+mark_as_advanced(Poco_Net_INCLUDE_DIR)
+mark_as_advanced(Poco_Net_LIBRARY_DEBUG)
+mark_as_advanced(Poco_ROOT_DIR)
+mark_as_advanced(Poco_Util_INCLUDE_DIR)
+mark_as_advanced(Poco_Util_LIBRARY_DEBUG)
